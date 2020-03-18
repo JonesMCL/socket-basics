8b1d0dad6620c864a9d8979f33ecc7a7a4d259fd
Jonas Lackmann <lackmann.jonas@gmail.com>
2020-03-18 15:02:50 +0100

    Add front-end form to submit message

--- public/js/app.js
+++ public/js/app.js
@@ -8,3 +8,9 @@ socket.on("message", function (message) {
 	console.log("New message: ");
 	console.log(message.text);
 })
+
+function submit() {
+	var fetchedStr = document.getElementById("text1").value;
+	socket.emit("message", {text: fetchedStr});
+	document.getElementById("text1").value = "";
+}
