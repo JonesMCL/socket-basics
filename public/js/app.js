466f49d7bdad65ab953d4103c5c4fb3fc2b0e295
Jonas Lackmann <lackmann.jonas@gmail.com>
2020-03-19 11:35:55 +0100

    Add name support

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,8 @@
+//var name = getQueryVariable("name");
+//var room = getQueryVariable("room");
 var socket = io();
+
+//console.log(name + " wants to join " + room);
 
 socket.on("connect", function () {
 	console.log("connected to socket.io server!");
@@ -6,14 +10,22 @@ socket.on("connect", function () {
 
 socket.on("message", function (message) {
 	var momentTimestamp = moment.utc(message.timeStamp);
+	//var $(message) = jQuery('.messages');
+
 	console.log("New message: ");
 	console.log(message.text);
-
-	jQuery(".messages").append('<p><strong>' + momentTimestamp.local().format('h:mm a') + ': </strong>' + message.text + '</p>');
+	
+	jQuery(".messages").append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mm a') + '</strong></p>');
+	//console.log("jQuery Messages: " + jQuery(".messages").name + "message.name: " + message.name);
+	//jQuery(".messages").append('<p><strong>' + momentTimestamp.local().format('h:mm a') + ': </strong>' + message.text + '</p>');
+	jQuery(".messages").append('<p>' + message.text + '</p>');
 })
 
 function submit() {
+	var urlParams = new URLSearchParams(window.location.search);
+	var messageFrom = urlParams.get("name");
+	// urlPrams und messageForm liefert richtiges Ergebnis
 	var fetchedStr = document.getElementById("text1").value;
-	socket.emit("message", {text: fetchedStr});
+	socket.emit("message", {text: fetchedStr, name: messageFrom});
 	document.getElementById("text1").value = "";
 }
