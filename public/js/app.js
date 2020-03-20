ec8b0f813d3369c5a1dff6d5d3c412f0ab61ed3e
Jonas Lackmann <lackmann.jonas@gmail.com>
2020-03-20 13:10:51 +0100

    Add styles for chat app

--- public/js/app.js
+++ public/js/app.js
@@ -1,39 +1,45 @@
-var name = getQueryVariable("name") || "Anonymous";
-var room = getQueryVariable("chatroom");
-console.log("fetched Room: " + room);
-console.log("fetched name: " + name);
+var name = getQueryVariable('name') || 'Anonymous';
+var room = getQueryVariable('chatroom');
 var socket = io();
-
-//Update h1 tag for room title
-jQuery('.room-title-test').text(room);
 
 console.log(name + ' wants to join ' + room);
 
-socket.on("connect", function () {
-	console.log("connected to socket.io server!");
-	socket.emit("joinRoom", {
+// Update h1 tag
+jQuery('.room-title').text(room);
+
+socket.on('connect', function () {
+	console.log('Conncted to socket.io server!');
+	socket.emit('joinRoom', {
 		name: name,
 		room: room
 	});
 });
 
-socket.on("message", function (message) {
-	var momentTimestamp = moment.utc(message.timeStamp);
-	//var $(message) = jQuery('.messages');
+socket.on('message', function (message) {
+	var momentTimestamp = moment.utc(message.timestamp);
+	var $messages = jQuery('.messages');
+	var $message = jQuery('<li class="list-group-item"></li>');
 
-	console.log("New message: ");
+	console.log('New message:');
 	console.log(message.text);
-	
-	jQuery(".messages").append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mm a') + '</strong></p>');
-	jQuery(".messages").append('<p>' + message.text + '</p>');
-})
 
-function submit() {
-	var urlParams = new URLSearchParams(window.location.search);
-	var messageFrom = urlParams.get("name");
-	var roomName = urlParams.get("chatroom");
-	// urlPrams und messageForm liefert richtiges Ergebnis
-	var fetchedStr = document.getElementById("text1").value;
-	socket.emit("message", {text: fetchedStr, name: messageFrom, room: roomName});
-	document.getElementById("text1").value = "";
-}
+	$message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mm a') + '</strong></p>');
+	$message.append('<p>' + message.text + '</p>');
+	$messages.append($message);
+});
+
+// Handles submitting of new message
+var $form = jQuery('#message-form');
+
+$form.on('submit', function (event) {
+	event.preventDefault();
+
+	var $message = $form.find('input[name=message]');
+
+	socket.emit('message', {
+		name: name,
+		text: $message.val()
+	});
+
+	$message.val('');
+});
